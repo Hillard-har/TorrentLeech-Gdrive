bc2e118d5decd97081e73a28feb70835f5f65d49
Mʀ. A <73195851+Hillard-har@users.noreply.github.com>
2021-06-05 12:53:00 +0530

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,23 @@
+from tobrot.sample_config import Config
+
+
+class Config(Config):
+
+    TG_BOT_TOKEN = "1682428637:AAH0mqIq4V761Nq7JV0gag1Qvv1kq6TWJOE"
+    APP_ID = 3264452
+    API_HASH = "dfa13ad72701f1bbaba18eb59fd52df3"
+    #OWNER_ID = 1416381241
+    AUTH_CHANNEL = [-1001206538636, 1416381241]
+    INDEX_LINK = ""
+    GLEECH_COMMAND = "gleech@Tetristest66_bot"
+    YTDL_COMMAND = "ytdl@Tetristest66_bot"
+    TELEGRAM_LEECH_COMMAND_G = "tleech@Tetristest66_bot"
+    CLONE_COMMAND_G = "gclone@Tetristest66_bot"
+    PYTDL_COMMAND_G = "pytdl@Tetristest66_bot"
+    DESTINATION_FOLDER = "Gdrive bot"
+    LEECH_COMMAND = "leech@Tetristest66_bot"
+    CANCEL_COMMAND_G = "cancel@Tetristest66_bot"
+    RCLONE_CONFIG = """""
+
+
+
